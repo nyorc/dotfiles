124139cb06b4db50a4eb00cd1ef26db5f4ef008c
nyo <nyo4rdace@gmail.com>
2020-02-15 21:46:05 +0800

    add terraform autocomplete

--- zsh/develop.zsh
+++ zsh/develop.zsh
@@ -24,3 +24,8 @@ if [ -d "$HOME/.nvm/" ]; then
     [ -s "$NVM_DIR/nvm.sh"  ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
+
+if [ -e "/usr/local/bin/terraform" ]; then
+    autoload -U +X bashcompinit && bashcompinit
+    complete -o nospace -C /usr/local/bin/terraform terraform
+fi
