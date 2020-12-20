16cd5e128101effef2b0d8a60472a07cb43e5e8d
nyo <nyo4rdace@gmail.com>
2020-12-20 22:10:50 +0800

    update PATH for Google Cloud SDK

--- zsh/develop.zsh
+++ zsh/develop.zsh
@@ -30,3 +30,9 @@ if [ -d "$HOME/.local/bin" ]; then
         PATH="$HOME/.local/bin:$PATH"
     fi
 fi
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f "$HOME/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/google-cloud-sdk/path.zsh.inc"; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f "$HOME/google-cloud-sdk/completion.zsh.inc" ]; then . "$HOME/google-cloud-sdk/completion.zsh.inc"; fi
