ccacd4b1187c447a5f3ba0c4396298d67701e1dd
nyo <nyo4rdace@gmail.com>
2020-12-27 19:53:01 +0800

    use ansible as install manage tool

--- installation/linux/basic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-sudo apt install -y \
-    git git-doc tig \
-    curl htop whois xclip lm-sensors \
-    jq feh w3m-img \
-    dnsutils
--- installation/linux/thefuck.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-sudo apt install python3-dev python3-pip
-sudo pip3 install thefuck
