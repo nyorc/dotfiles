cfba41fcf0d27ac9c2e85787b5e633c7a04846fb
nyo <nyo4rdace@gmail.com>
2022-11-27 01:15:54 +0800

    rewrite php installation with ansible

--- installation/mac/php.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-brew install php
-brew install composer
-
-composer global require "squizlabs/php_codesniffer=*"
-composer global require "phpmd/phpmd=@stable"
-composer global require "friendsofphp/php-cs-fixer"
