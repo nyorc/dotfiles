5dc8a8e54fbfba7204dc64693193ea61f2d8d00a
nyo <nyo4rdace@gmail.com>
2022-12-04 20:33:46 +0800

    remove rewrited script

--- installation/linux/diff-so-fancy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# diff-so-fancy
-# source: https://github.com/so-fancy/diff-so-fancy
-
-mkdir -p ~/bin
-wget https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
-chmod +x diff-so-fancy
-mv diff-so-fancy ~/bin
